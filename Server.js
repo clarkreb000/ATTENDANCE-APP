dbb5b265eb88dc91a6d3495e8b75ae12c144428c
browntre000 <browntre000@warren.k12.in.us>
2019-09-05 10:59:22 -0400

    i love jesus

--- Server.js
+++ Server.js
@@ -1,7 +1,21 @@
-var http = require('http');
+let http = require('http');
+let fs = require('fs');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-    console.log('This example is different!');
-}).listen(8080);
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('./MAIN.html', null, function(error, data){
+        if(error){
+            res.writeHead(404);
+            res.write('Whoops! FIle not Found!');
+        }
+        else{
+            res.write(data);
+        }
+        res.end();
+    })
+}).listen(8080, '127.0.0.1');
+
+// http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.end('Hello World!');
+// }).listen(8080, '127.0.0.1');
